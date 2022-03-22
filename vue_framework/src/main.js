7bc05bcf3a000127ddcdd40aa43d089c476801e5
JEONGJAE <jeongjae4050@gmail.com>
2022-03-22 10:58:33 +0900

    [BalmUI] 주석 추가

--- vue_framework/src/main.js
+++ vue_framework/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import BalmUI from 'balm-ui'; // Official Google Material Components
 import BalmUIcss from 'balm-ui/dist/balm-ui.css';
-
+//https://material.balmjs.com/
 const app = createApp(App);
 
 app.use(BalmUI); // Mandatory
