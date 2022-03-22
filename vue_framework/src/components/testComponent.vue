3ce1053922fcbf8276039f711f1cbf41a347da67
JEONGJAE <jeongjae4050@gmail.com>
2022-03-22 11:14:30 +0900

    [AntDesignVue] 기본 설정 완료

--- vue_framework/src/components/testComponent.vue
+++ vue_framework/src/components/testComponent.vue
@@ -1,5 +1,11 @@
 <template>
-	<div>여기에 테스트 코딩</div>
+	<div>
+		<a-button type="primary">Primary Button</a-button>
+		<a-button>Default Button</a-button>
+		<a-button type="dashed">Dashed Button</a-button>
+		<a-button type="text">Text Button</a-button>
+		<a-button type="link">Link Button</a-button>
+	</div>
 </template>
 
 <script>
