489efa5b47036a4dabe9a382bd2b0d9bada5c721
JEONGJAE <jeongjae4050@gmail.com>
2022-03-22 13:46:23 +0900

    [AntDesignVue] 단일 모듈 로드 설정

--- vue_framework/src/main.js
+++ vue_framework/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import Antd from 'ant-design-vue';
+// import Antd from 'ant-design-vue';
+import { Button } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 //https://next.antdv.com/index
 
 const app = createApp(App);
 
-app.use(Antd);
+// app.use(Antd);
+app.use(Button);
 app.mount('#app');
